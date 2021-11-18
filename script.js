b266c39d75d45e279f35882fd9e4253b72691314
Shibaken28 <8128kanzensu@gmail.com>
2021-11-18 22:30:00 +0900

    ステータスを変更できるように

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(800, 800) 
-  background(255);
+  createCanvas(800, 800);
   vars=4;//変数の数
   makeTables();
+  mouseState="up";
 }
 
 
@@ -12,6 +12,7 @@ function makeTables(){
     bitState[i]=0;
   }
   table = new Array((1<<vars)+1);
+  //allowToWrite = new Array((1<<vars)+1);
   for(var i=0;i<table.length;i++){
     table[i] = new Array(vars+1);
     for(var j=0;j<vars+1;j++){
@@ -20,10 +21,13 @@ function makeTables(){
         if(j==vars)table[i][j]="A";
       }else{
         if(j<vars)table[i][j]=((i-1)&(1<<(vars-1-j)))?"1":"0";
-        else table[i][j]=str(bitState[j]);
       }
     }
   }
+  //vars=4のときのみ使える
+  idX=[0b00,0b01,0b11,0b10];
+  idY=[0b00,0b01,0b11,0b10];
+  //使い方 T[x][y]=bitState[idX[x]|idY[y]<<2]
 }
 
 function drawTable(Arr,sx,sy,wid,hei){
@@ -34,17 +38,61 @@ function drawTable(Arr,sx,sy,wid,hei){
   for(var h=0;h<cy;h++){
     for(var w=0;w<cx;w++){
       noFill();
+      strokeWeight(1);
       stroke(0);
       rect(sx+dx*w,sy+dy*h,dx,dy);
       fill(0);
+      noStroke(0);
       textSize(20);
-      textAlign(LEFT,TOP);
-      text(str(Arr[h][w]), sx+dx*w,sy+dy*h);
+      textAlign(CENTER,CENTER);
+      text(str(Arr[h][w]), sx+dx*w+dx/2,sy+dy*h+dy/2);
     }
   }
 }
 
+function isInRect(sx,sy,dx,dy,ax,ay){
+  return (sx<=ax&&ax<=sx+dx&&sy<=ay&&ay<=sy+dy);
+}
+
+function flipFlop(Arr,sx,sy,wid,hei){
+  //ハードコーディングごめん
+  var cx=Arr[0].length;
+  var cy=Arr.length;
+  var dx=wid/cx;
+  var dy=hei/cy;
+  for(var h=1;h<cy;h++){
+    var w=cx-1;
+    noFill();
+    stroke(0);
+    strokeWeight(2);
+    rect(sx+dx*w,sy+dy*h,dx,dy);
+    if(mouseState=="down"&&isInRect(sx+dx*w,sy+dy*h,dx,dy,mouseX,mouseY)){
+      bitState[h-1]=1-bitState[h-1];
+    }
+  }
+}
+
+function updateMouseState(){
+  if(mouseIsPressed){
+    if(mouseState=="up"){
+      mouseState="down";
+    }else{
+      mouseState="pressed";
+    }
+  }else{
+    mouseState="up";
+  }
+}
+
 function draw() {
-  drawTable(table,10,10,140,600);
+  clear();
+  background(255);
+  updateMouseState();
+  var sx=10,sy=10,wid=200,hei=500;
+  drawTable(table,sx,sy,wid,hei);
+  flipFlop(table,sx,sy,wid,hei);
+  for(var i=0;i<bitState.length;i++){
+    table[i+1][vars]=str(bitState[i]);
+  }
 }
 
